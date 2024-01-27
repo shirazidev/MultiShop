05812bd780498e7aec70356106a8f51db00ffd29
AmirHossein Shirazi <ashzih2@gmail.com>
2024-01-28 01:08:53 +0330

    remove old account app

--- account/testsms.py
+++
@@ -1,8 +0,0 @@
-import ghasedakpack
-
-sms = ghasedakpack.Ghasedak("5d6cefad1e7e9dbddb66e90f224cde86391600b9d1f7254baef7b98c52af4770")
-try:
-    sms.verification({'receptor': '09385860444','type': '1','template': 'djangoetebar','param1': '1612'})
-    print("done")
-except:
-    print("failed")
--- account/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "Account"
-urlpatterns = [
-    path('login', views.LoginView.as_view(), name='login'),
-    path('register', views.RegisterView.as_view(), name='register'),
-    path('register/checkotp', views.CheckOtpView.as_view(), name='checkotp'),
-]
